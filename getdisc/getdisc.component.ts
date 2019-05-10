e43eec86b36f42ef74bd329684c6627a85d7a43f
prathyusha-g <45353578+prathyusha-g@users.noreply.github.com>
2019-05-10 21:58:56 +0530

    Add files via upload

---
+++ getdisc/getdisc.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-getdisc',
+  templateUrl: './getdisc.component.html',
+  styleUrls: ['./getdisc.component.css']
+})
+export class GetdiscComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
